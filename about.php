844bf54c96fc9aaf221df723ced46650f7d029bb
Prince Rai <princerai162@gmail.com>
2019-10-08 13:04:04 +0530

    about page addedd

---
+++ about.php
@@ -0,0 +1,177 @@
+<?php
+
+include ('header.php');?>
+
+<main>
+  <section id="subheader" data-speed="8" data-type="background" >
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center"> ABOUT US </h1>
+        </div>
+      </div>
+    </div>
+    <div class="crumb">
+      <div class="container">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li class="sep">/</li>
+          <li>ABOUT US</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <!--subheader-->
+  <div id="content" class="no-top no-bottom">
+    <!--about-us-->
+    <section id="section-about" class="side-bg no-padding">
+      <div class="container">
+        <div class="row">
+          
+          <div class="col-md-6 col-sm-6  text-center div-right"> <img src="assets/img/bg-side-1.jpg" class="img-responsive" alt="img" title="img" /> </div>
+<div class="col-md-6 col-sm-6 ">
+            <h2>Best Interior Design </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="mb">aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+          </div>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </section>
+    <!--about-us-->
+  </div>
+  <!--testimonial-->
+  <section id="section-testimonial" class="text-light">
+    <div>
+      <h2 class="text-center"> Customer Says </h2>
+      <div class="separator"><span><i class="fa fa-circle"></i></span></div>
+    </div>
+    <div class="container content">
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <div class="row">
+              <div class="col-md-6  col-sm-6">
+                <blockquote class="custom-quote">
+                  <div class="row">
+                    <div class="col-md-4  col-sm-4 text-center"><img src="assets/img/img1.jpg" alt="img" title="img" class="img-responsive img-circle"></div>
+                    <div class="col-md-8  col-sm-8"> <span class="fa fa-quote-left"></span>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cons
+                        entum tempor. Duis porta lectus ut dui consequat, </p>
+                      <footer>
+                        <h6>JOHNATHAN DOE <span>DESIGNER, THEME DESIGNER</span></h6>
+                      </footer>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+              <div class="col-md-6  col-sm-6  slider-n">
+                <blockquote class="custom-quote">
+                  <div class="row">
+                    <div class="col-md-4  col-sm-4 text-center"><img src="assets/img/img2.jpg" alt="img" title="img" class="img-responsive img-circle"></div>
+                    <div class="col-md-8  col-sm-8"> <span class="fa fa-quote-left"></span>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cons
+                        entum tempor. Duis porta lectus ut dui consequat, </p>
+                      <footer>
+                        <h6>JOHNATHAN DOE <span>DESIGNER, THEME DESIGNER</span></h6>
+                      </footer>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="row">
+              <div class="col-md-6 col-sm-6">
+                <blockquote class="custom-quote">
+                  <div class="row">
+                    <div class="col-md-4 col-sm-4 text-center"><img src="assets/img/img3.jpg" title="" alt="" class="img-responsive img-circle"></div>
+                    <div class="col-md-8 col-sm-8"> <span class="fa fa-quote-left"></span>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cons
+                        entum tempor. Duis porta lectus ut dui consequat, </p>
+                      <footer>
+                        <h6>JOHNATHAN DOE <span>DESIGNER, THEME DESIGNER</span></h6>
+                      </footer>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+              <div class="col-md-6 col-sm-6  slider-n">
+                <blockquote class="custom-quote">
+                  <div class="row">
+                    <div class="col-md-4 col-sm-4 text-center" ><img src="assets/img/img4.jpg" alt="img" title="img" class="img-responsive  img-circle"></div>
+                    <div class="col-md-8 col-sm-8"> <span class="fa fa-quote-left"></span>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cons
+                        entum tempor. Duis porta lectus ut dui consequat, </p>
+                      <footer>
+                        <h6>JOHNATHAN DOE <span>DESIGNER, THEME DESIGNER</span></h6>
+                      </footer>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="row">
+              <div class="col-md-6 col-sm-6">
+                <blockquote class="custom-quote">
+                  <div class="row">
+                    <div class="col-md-4 col-sm-4 text-center"><img src="assets/img/img1.jpg" alt="img" title="img" class="img-responsive img-circle"></div>
+                    <div class="col-md-8 col-sm-8"> <span class="fa fa-quote-left"></span>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cons
+                        entum tempor. Duis porta lectus ut dui consequat, </p>
+                      <footer>
+                        <h6>JOHNATHAN DOE <span>DESIGNER, THEME DESIGNER</span></h6>
+                      </footer>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+              <div class="col-md-6 col-sm-6  slider-n">
+                <blockquote class="custom-quote">
+                  <div class="row">
+                    <div class="col-md-4 col-sm-4 text-center"><img src="assets/img/img2.jpg" alt="img" title="img" class="img-responsive img-circle"></div>
+                    <div class="col-md-8 col-sm-8"> <span class="fa fa-quote-left"></span>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cons
+                        entum tempor. Duis porta lectus ut dui consequat, </p>
+                      <footer>
+                        <h6>JOHNATHAN DOE <span>DESIGNER, THEME DESIGNER</span></h6>
+                      </footer>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--testimonial-->
+  <!--Contact us-->
+  <section id="call-to-action" class="call-to-action bg-color dark text-center">
+    <h6><a href="contact-us.html" class="btn btn-line-black btn-big hvr-sweep-to-left">Contact us</a></h6>
+  </section>
+  <!--Contact us-->
+</main>
+<!--main-->
+
+</body>
+
+<?php
+
+include('footer.php');
+
+?>
+
+
